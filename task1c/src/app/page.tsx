3e124e6c9212b3e81eb72d8a67715f538c08df17
lyklecharova <lklecharova9@gmail.com>
2024-04-21 20:47:17 +0300

    Create page.tsx

---
+++ task1c/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header/Header";
+import { OtherServices } from "@/components/OtherServices/OtherServices";
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Header/>
+            <OtherServices/>
+        </main>
+    );
+}
