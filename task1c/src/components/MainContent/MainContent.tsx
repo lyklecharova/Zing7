9995d7b5edb0c1bdd3333a451b474c14a18aa75a
lyklecharova <lklecharova9@gmail.com>
2024-04-22 09:46:12 +0300

    Create MainContent.tsx

---
+++ task1c/src/components/MainContent/MainContent.tsx
@@ -0,0 +1,35 @@
+export const MainContent = () => {
+    return (
+        <>
+        <div className="flex flex-col items-start justify-center">
+           <h1 className="mt-4 text-left font-grotesk text-xl font-semibold text-dark-blue sm:text-4xl md:mt-[72px] md:leading-[108%] md:-tracking-[4px] lg:text-5xl xl:text-6xl 2xl:text-7xl" style={{ color: '#01303A' }}>Решения</h1>
+            <div className="mt-6 max-w-[80%] lg:mt-[64px] text-left">
+                <p>Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки местни предприятия до мултинационални компании.</p>
+            </div>
+            <div className="mt-6 max-w-[80%] lg:mt-[64px] text-left">
+                <p>Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.</p>
+                <ul className="text-left list-disc">
+                    <li>Решения за всички видове стоки (конвенционални, изискващи температурен режим, опасни товари)</li>
+                    <li>Превоз на стоки от и до Балканите и всички страни на ЕС, цели и частични товари;
+                    Взимане на стоки и доставки за от и в България;</li>
+                    <li>Съчетаване на дълги и къси дестинации за ефективно планиране и разходи;
+                    Регулярни групажни линии;</li>
+                    <li>Транспортни услуги до Турция и Близкия изток;</li>
+                    <li>Спазване на изискванията за качество и стандарти на клиентите и/или на техни специфични изисквания.</li>
+                </ul>
+            </div>
+        </div>
+
+        <div className="flex flex-col items-start justify-center">
+           <h1 className="mt-4 text-left font-grotesk text-xl font-semibold text-dark-blue sm:text-4xl md:mt-[72px] md:leading-[108%] md:-tracking-[4px] lg:text-5xl xl:text-6xl 2xl:text-7xl" style={{ color: '#01303A' }}>Предимства</h1>
+            <div className="mt-6 max-w-[80%] lg:mt-[64px] text-left">
+                <p>Фокусът ни върху клиентите е същността на нашата работа. Ние доставяме услуги за всички видове бизнес – от малки местни предприятия до мултинационални компании.</p>
+            </div>
+            <div className="mt-6 max-w-[80%] lg:mt-[64px] text-left">
+                <p>Ние обработваме вашата пратка като всяка друга голяма товарна пратка – старателно и професионално.</p>
+            </div>
+        </div>
+        </>
+        
+    );
+}
