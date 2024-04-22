2f4015eef9a5092bd2214db09c2e72eb4ff5f07b
lyklecharova <lklecharova9@gmail.com>
2024-04-22 09:46:07 +0300

    Update page.tsx

--- task1c/src/app/page.tsx
+++ task1c/src/app/page.tsx
@@ -1,13 +1,18 @@
 import { Header } from "@/components/Header/Header";
 import { HeroSection } from "@/components/HeroSection/HeroSection";
+import { MainContent } from "@/components/MainContent/MainContent";
 import { OtherServices } from "@/components/OtherServices/OtherServices";
+import { TextContent } from "@/components/TextContent/TextContent";
+
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{width : '1600px', height : '3523'}}>
-            <Header/>
-            <HeroSection/>
-            <OtherServices/>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24" style={{ width: '1600px', height: '3523' }}>
+            <Header />
+            <HeroSection />
+            <MainContent />
+            <TextContent />
+            <OtherServices />
         </main>
     );
 }
