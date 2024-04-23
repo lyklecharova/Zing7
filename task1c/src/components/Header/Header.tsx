9abfb5053bce087740a8832378bda12f004a300c
lyklecharova <lklecharova9@gmail.com>
2024-04-23 11:23:05 +0300

    Update Header.tsx

--- task1c/src/components/Header/Header.tsx
+++ task1c/src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
   return (
-    <nav className="flex fixed w-screen justify-center items-center text-center" >
-      <div className="flex items-center justify-between" style={{ width: "1308px"}}>
+    <nav className="p-4 flex justify-center" style={{ width: "100%", height: '51px' }}>
+      <div className="flex items-center justify-between" style={{ width: "1308px" }}>
         <div className="flex items-center">
-          <h1 className="text-custom-color text-xl font-bold" style={{ lineHeight: '140%', verticalAlign: 'middle'}}>
+          <h1 className="text-custom-color text-xl font-bold" style={{ lineHeight: '140%', verticalAlign: 'middle' }}>
             Website
           </h1>
         </div>
@@ -25,13 +25,14 @@ export const Header = () => {
           </li>
         </ul>
         <div className="flex items-center">
-          <button className="mr-3" style={{ width: "56px", height: '27px'}}>
+          <button className="mr-3" style={{ width: "56px", height: '27px' }}>
             <a href="#" className="text-gray-800 hover:text-gray-300">Профил</a>
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md" 
-            style={{ backgroundColor: '#01303A'}}>
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold color:text-white py-2 px-4 md:px-15 border-8 border-blue-500 rounded-lg"
+            style={{ color: '#01303A', margin: '0 10px', backgroundColor: '#fff', borderColor: '#01303A' }}>
             Заявка за пратка
           </button>
+
         </div>
       </div>
     </nav>
