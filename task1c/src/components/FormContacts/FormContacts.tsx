c307ea5da28d549f4655ef7046167ac16a582d45
lyklecharova <lklecharova9@gmail.com>
2024-04-23 12:37:11 +0300

    Update FormContacts.tsx

--- task1c/src/components/FormContacts/FormContacts.tsx
+++ task1c/src/components/FormContacts/FormContacts.tsx
@@ -2,29 +2,21 @@ import React from 'react';
 
 export const FormContacts = () => {
     return (
-        <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full md:w-full px-3 mb-6 md:mb-0 relative">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name" style={{ width: '290px', height: '72px', lineHeight: '36px' }}>
-                        Мобилен телефон
-                    </label>
-                    <div className="relative">
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                            <svg className="h-[12px] w-[12px] text-slate-500" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                <rect x="3" y="5" width="18" height="14" rx="2" />
-                                <polyline points="3 7 12 13 21 7" />
-                            </svg>
-                        </span>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 pl-10 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Мобилен телефон" />
-                        <button className="absolute inset-y-0 right-0 bg-yellow-400 hover:bg-yellow-500 text-gray-800 font-bold py-3 px-4 rounded-r" type="button" style={{ backgroundColor: '#E7F874' }}>
-                            Изпрати
-                        </button>
-                    </div>
-
+        <form className="w-full max-w-lg" style={{ width: '55%' }}>
+            <div className="flex flex-wrap -mx-2 mb-2 items-center ">
+                <div className="flex-1 relative">
+                    <span className="absolute inset-y-0 left-2 flex items-center pl-2">
+                        <svg className="h-[12px] w-[12px] text-slate-500" width="20" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <rect x="3" y="5" width="18" height="14" rx="2" />
+                            <polyline points="3 7 12 13 21 7" />
+                        </svg>
+                    </span>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 pl-10 pr-12 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Мобилен телефон" style={{ borderWidth: '10px' }} />
+                    <button className="absolute right-0.2 top-0.5 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold text-white py-2 px-4 border border-blue-500 border-transparent rounded" type="button" style={{ background: '#E7F874', color: '#083C2F', borderWidth: '10px', padding: '5px 10px', top: "5px" }}>
+                        Изпрати
+                    </button>
                 </div>
-
             </div>
         </form>
     );
 }
-
