e6df26629be50aa3663794f75695528fa4257e44
lyklecharova <lklecharova9@gmail.com>
2024-04-23 14:18:14 +0300

    Update TextContent.tsx

--- task1c/src/components/TextContent/TextContent.tsx
+++ task1c/src/components/TextContent/TextContent.tsx
@@ -5,10 +5,10 @@ export const TextContent = () => {
     return (
         <section className="mt-12 flex w-full items-center justify-center py-12 text-center bg-dark-blue-700 lg:mt-[100px] lg:py-[100px]" style={{ backgroundColor: '#394649' }}>
             <article className="flex w-[80%] flex-col items-center xl:w-[40%] p-8 rounded-lg">
-                <h2 className="font-grotesk font-semibold text-white text-2xl md:text-5xl md:leading-[108%] md:-tracking-[4px] lg:text-5xl xl:text-6xl 2xl:text-7xl">
+                <h2 className="font-grotesk font-semibold text-black text-2xl md:text-5xl md:leading-[108%] md:-tracking-[4px] lg:text-5xl xl:text-6xl 2xl:text-7xl">
                     Готови ли сте да работим заедно?
                 </h2>
-                <p style={{ maxWidth: '80%', marginTop: '0.75rem', width: '684px', height: '68px', color: '#FFFFFF', fontSize: '16px' }}>
+                <p style={{ maxWidth: '80%', marginTop: '0.75rem', width: '684px', height: '68px', color: '#0000', fontSize: '16px' }}>
                     Оставете вашият телефон и ние ще се свържем с вас, за да отговорим на всички въпроси
                 </p>
                 <FormContacts />
